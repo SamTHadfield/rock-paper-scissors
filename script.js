85bde57c09cea85ffed232a7608c7cedaac61a8a
Sam Hadfield <sthtech@protonmail.com>
2024-10-10 17:02:12 -0500

    Create logic for human choice
    - Create a new function named getHumanChoice
    - Create let variable within function
    - Set let variable equal to prompt input
    - Create if/else statements within function
    - Base output on prompt input

--- script.js
+++ script.js
@@ -6,16 +6,41 @@
 // Use Math.random method on variable to generate a random number
 // Employ if/else statements to print string based on random output of variable
 
-function getComputerChoice() {
-  return Math.random();
+// function getComputerChoice() {
+//   return Math.random();
+// }
+
+// let choice = getComputerChoice();
+
+// if (choice >= 0 && choice <= 0.33) {
+//   console.log("rock");
+// } else if (choice >= 0.34 && choice <= 0.66) {
+//   console.log("paper");
+// } else {
+//   console.log("scissors");
+// }
+
+// Step 3: Write the logic to get the human choice (pseudocode)
+
+// Create a new function named getHumanChoice
+// Create let variable within function
+// Set let variable equal to prompt ("Do you choose 'rock', 'paper', or 'scissors'?")
+// Create if/else statements within function based on value of let variable
+
+function getHumanChoice() {
+  let choice = prompt(
+    "Do you choose 'rock', 'paper', or 'scissors'?"
+  ).toLowerCase();
+
+  if (choice === "rock") {
+    console.log("rock");
+  } else if (choice === "paper") {
+    console.log("paper");
+  } else if (choice === "scissors") {
+    console.log("scissors");
+  } else {
+    console.log("I'm sorry, you entered an incorrect value");
+  }
 }
 
-let choice = getComputerChoice();
-
-if (choice >= 0 && choice <= 0.33) {
-  console.log("rock");
-} else if (choice >= 0.34 && choice <= 0.66) {
-  console.log("paper");
-} else {
-  console.log("scissors");
-}
+getHumanChoice();

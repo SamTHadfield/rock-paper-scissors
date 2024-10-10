29452da9e1bf5a9356190d47fd718e63475a9ab3
Sam Hadfield <sthtech@protonmail.com>
2024-10-10 15:21:32 -0500

    Write pseudocode for Step 2

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+// Step 2: Write the logic to get the computer choice
+
+// Create new function named getComputerChoice()
+// Will need to randomly return one of the following strings: "rock", "paper", "scissors"
+// Declare variable
+// Use Math.random method on variable to generate a random number
+// Employ if/else statements to print string based on random output of variable

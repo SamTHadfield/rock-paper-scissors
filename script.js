a6056f32af6e909e1dd44fcbffadd0e7a8f32c03
Sam Hadfield <sthtech@protonmail.com>
2024-10-10 17:20:54 -0500

    Add variables for humanScore and computerScore

--- script.js
+++ script.js
@@ -6,20 +6,6 @@
 // Use Math.random method on variable to generate a random number
 // Employ if/else statements to print string based on random output of variable
 
-// function getComputerChoice() {
-//   return Math.random();
-// }
-
-// let choice = getComputerChoice();
-
-// if (choice >= 0 && choice <= 0.33) {
-//   console.log("rock");
-// } else if (choice >= 0.34 && choice <= 0.66) {
-//   console.log("paper");
-// } else {
-//   console.log("scissors");
-// }
-
 // Step 3: Write the logic to get the human choice (pseudocode)
 
 // Create a new function named getHumanChoice
@@ -27,6 +13,22 @@
 // Set let variable equal to prompt ("Do you choose 'rock', 'paper', or 'scissors'?")
 // Create if/else statements within function based on value of let variable
 
+// Step 2
+function getComputerChoice() {
+  return Math.random();
+}
+
+let choice = getComputerChoice();
+
+if (choice >= 0 && choice <= 0.33) {
+  console.log("rock");
+} else if (choice >= 0.34 && choice <= 0.66) {
+  console.log("paper");
+} else {
+  console.log("scissors");
+}
+
+// Step 3
 function getHumanChoice() {
   let choice = prompt(
     "Do you choose 'rock', 'paper', or 'scissors'?"
@@ -44,3 +46,7 @@ function getHumanChoice() {
 }
 
 getHumanChoice();
+
+// Step 4
+let humanScore = 0;
+let computerScore = 0;

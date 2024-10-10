ba8710819359d4767c68dfd5fbe4c618ee11e13f
Sam Hadfield <sthtech@protonmail.com>
2024-10-10 16:20:59 -0500

    Declare function, variable, and if/else statement
    - Declare function
    - Set return of function call to generate random
    number
    - Store function return in variable
    - Create if/else statement to print string based
    on variable result.

--- script.js
+++ script.js
@@ -5,3 +5,17 @@
 // Declare variable
 // Use Math.random method on variable to generate a random number
 // Employ if/else statements to print string based on random output of variable
+
+function getComputerChoice() {
+  return Math.random();
+}
+
+let choice = getComputerChoice();
+
+if (choice >= 0 && choice <= 0.33) {
+  console.log("rock");
+} else if (choice >= 0.34 && choice <= 0.66) {
+  console.log("paper");
+} else {
+  console.log("scissors");
+}

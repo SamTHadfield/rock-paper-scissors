dbb1b4e655a61709fc846e8a09dc577dc77891c2
Sam Hadfield <sthtech@protonmail.com>
2024-12-17 09:48:33 -0600

    Pare down switch statement, breaking off into sep- arate helper functions. Capitalize strings. Set gameWinner() function to remove buttons once game winner is determined.

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-//Choice functions
+/////////////////
+// Scoreboard //
+////////////////
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = document.getElementById("round-winner");
+let currentScore = document.getElementById("current-score");
+
+///////////////////////////////////////////
+// Player Selection | Computer Selection //
+//////////////////////////////////////////
+
+// Add Event Listeners | Call playRound() function | Pass in arguments
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const playerSelection = button.id;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+  });
+});
+
 function getComputerChoice() {
   let choice = Math.random();
-
   if (choice >= 0 && choice <= 0.33) {
     return "rock";
   } else if (choice >= 0.34 && choice <= 0.66) {
@@ -11,6 +32,104 @@ function getComputerChoice() {
   }
 }
 
+////////////////
+// One Round //
+////////////////
+
+function playRound(playerChoice, computerChoice) {
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === "rock") {
+        tie();
+      } else if (computerChoice === "paper") {
+        computerWin();
+      } else if (computerChoice === "scissors") {
+        playerWin();
+      }
+      break;
+
+    case "paper":
+      if (computerChoice === "rock") {
+        playerWin();
+      } else if (computerChoice === "paper") {
+        tie();
+      } else if (computerChoice === "scissors") {
+        computerWin();
+      }
+      break;
+
+    case "scissors":
+      if (computerChoice === "rock") {
+        computerWin();
+      } else if (computerChoice === "paper") {
+        playerWin();
+      } else if (computerChoice === "scissors") {
+        tie();
+      }
+      break;
+  }
+
+  //////////////////////////////
+  // Capitalize First Letter //
+  //////////////////////////////
+
+  function playerChoiceCapitalized() {
+    const playerChoiceCapitalized =
+      playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+    return playerChoiceCapitalized;
+  }
+
+  function computerChoiceCapitalized() {
+    const computerChoiceCapitalized =
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    return computerChoiceCapitalized;
+  }
+
+  ///////////////////////
+  // Helper Functions //
+  //////////////////////
+
+  function playerWin() {
+    playerScore++;
+    roundWinner.innerText = `You win! ${playerChoiceCapitalized()} beats ${computerChoiceCapitalized()}!`;
+  }
+
+  function computerWin() {
+    computerScore++;
+    roundWinner.innerText = `You lose! ${computerChoiceCapitalized()} beats ${playerChoiceCapitalized()}!`;
+  }
+
+  function tie() {
+    roundWinner.innerText = "It's a draw!";
+  }
+
+  if (playerScore === 5 || computerScore === 5) {
+    gameWinner();
+  }
+
+  score();
+
+  function score() {
+    currentScore.innerText = `Player: ${playerScore} | Computer: ${computerScore}`;
+  }
+
+  function gameWinner() {
+    const container = document.getElementById("container");
+    container.remove();
+
+    if (playerScore === 5) {
+      roundWinner.innerText = "You won the game!";
+    } else if (computerScore === 5) {
+      roundWinner.innerText = "You lost to the computer!";
+    }
+  }
+}
+
+////////////////////////////////
+// OLD CODE FOR THIS PROJECT //
+///////////////////////////////
+
+// Human Choice Function:
 // function getHumanChoice() {
 //   let choice = prompt(
 //     "Do you choose 'rock', 'paper', or 'scissors'?"
@@ -25,81 +144,16 @@ function getComputerChoice() {
 //   }
 // }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+// Re-call functions to play 5 rounds:
+// for (let i = 0; i < 5; i++) {
+//   const humanSelection = getHumanChoice();
+//   const computerSelection = getComputerChoice();
+//   playRound(humanSelection, computerSelection);
+//   console.log("Human Score: " + humanScore);
+//   console.log("Computer Score: " + computerScore);
+// }
 
-  // Re-call functions to play 5 rounds
-  // for (let i = 0; i < 5; i++) {
-  //   const humanSelection = getHumanChoice();
-  //   const computerSelection = getComputerChoice();
-  //   playRound(humanSelection, computerSelection);
-  //   console.log("Human Score: " + humanScore);
-  //   console.log("Computer Score: " + computerScore);
-  // }
-
-  // Add Event Listeners to Buttons
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const humanSelection = button.id.toLowerCase();
-      const computerSelection = getComputerChoice();
-      playRound(humanSelection, computerSelection);
-    });
-  });
-
-  // One Round
-  function playRound(humanChoice, computerChoice) {
-    switch (humanChoice) {
-      case "rock":
-        if (computerChoice === "rock") {
-          console.log("It's a draw! Rock ties with Rock!");
-        } else if (computerChoice === "paper") {
-          console.log("You lose! Paper beats Rock!");
-          return computerScore++;
-        } else if (computerChoice === "scissors") {
-          console.log("You win! Rock beats Scissors!");
-          return humanScore++;
-        }
-        break;
-
-      case "paper":
-        if (computerChoice === "rock") {
-          console.log("You win! Paper beats Rock!");
-          return humanScore++;
-        } else if (computerChoice === "paper") {
-          console.log("It's a draw! Paper ties with Paper!");
-        } else if (computerChoice === "scissors") {
-          console.log("You lose! Scissors beats Paper!");
-          return computerScore++;
-        }
-        break;
-
-      case "scissors":
-        if (computerChoice === "rock") {
-          console.log("You lose! Rock beats Scissors!");
-          return computerScore++;
-        } else if (computerChoice === "paper") {
-          console.log("You win! Scissors beats Paper!");
-          return humanScore++;
-        } else if (computerChoice === "scissors") {
-          console.log("It's a draw! Scissors ties with Scissors!");
-        }
-        break;
-
-      default:
-        console.log("You entered an incorrect value.");
-    }
-  }
-
-  // Final message upon game completion
-  if (humanScore > computerScore) {
-    console.log("Congrats! You beat the Computer!");
-  } else if (humanScore === computerScore) {
-    console.log("Tie game!");
-  } else {
-    console.log("Sorry, the Computer wins the game.");
-  }
-}
-
-playGame();
+// console.log Statements:
+// console.log("It's a draw! Rock ties with Rock!");
+// console.log("You lose! Paper beats Rock!");
+// console.log("You win! Rock beats Scissors!");
